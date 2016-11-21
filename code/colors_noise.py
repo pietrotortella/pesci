2fd0c5df60da4e7621d8eecdaf7f3592d1efd06c
LucaBon <luca.bonura@gmail.com>
2016-11-21 11:13:28 +0100

    Updated version which runs from terminal

--- code/colors_noise.py
+++ code/colors_noise.py
@@ -3,7 +3,8 @@ from scipy import ndimage, misc
 from os import listdir
 from os.path import isfile, join
 import numpy as np
-import random
+import sys
+
 
 #add salt and pepper noise to the image
 #p is the probability of having salt noise and pepper noise (equal probability of having salt or pepper noise)
@@ -31,48 +32,51 @@ def changecolour(image_file,a,b,c):
     img_mod = np.swapaxes(img_mod, 1, 2)
     return img_mod, image_file
 
-# mypath is the path that contains the images
-# newpath is the path in which the new images will be stored
-mypath = '/home/terminale2/Downloads/train/DOL'
-newpath = mypath+'_color'
 
-# if the path already exists the software doesn't do anything, if it creates a new folder
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
+def Colors_noise(mypath):
+    # mypath is the path that contains the images
+    # newpath is the path in which the new images will be stored
+    newpath = mypath+'_color'
 
-# change the current directory to mypath
-os.chdir(mypath)
+    # if the path already exists the software doesn't do anything, if it creates a new folder
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
 
-#listdir returns a list with the content of the directory specified in mypath
-
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-#it cycles on every image inside the folder specified in mypath and save the modified images in the folder newpath
-for f in onlyfiles:
-    img_mod1, image_file = changecolour(f, np.random.random([1]), np.random.random([1]),np.random.random([1]))
-    os.chdir(newpath)
-    misc.imsave(image_file[0:-4] + '_col' + str(1) + '.jpg', img_mod1)
-
-    #print 'image ' + image_file[0:-4] + '_col' + str(1) + '.jpg created \n'
+    # change the current directory to mypath
     os.chdir(mypath)
 
-    img_mod2, image_file = changecolour(f, np.random.random([1]),np.random.random([1]),np.random.random([1]) )
-    os.chdir(newpath)
-    misc.imsave(image_file[0:-4] + '_col' + str(2) + '.jpg', img_mod2)
+    #listdir returns a list with the content of the directory specified in mypath
 
-    #print 'image ' + image_file[0:-4] + '_col' + str(2) + '.jpg created \n'
-    os.chdir(mypath)
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-    img_mod3, image_file = saltpepper(f, np.random.random([1]) * 0.05)
-    os.chdir(newpath)
-    misc.imsave(image_file[0:-4] + '_noise' + '.jpg', img_mod3)
-    #print 'image ' + image_file[0:-4] + '_noise' + '.jpg created \n'
-    os.chdir(mypath)
+    #it cycles on every image inside the folder specified in mypath and save the modified images in the folder newpath
+    for f in onlyfiles:
+        img_mod1, image_file = changecolour(f, np.random.random([1]), np.random.random([1]),np.random.random([1]))
+        os.chdir(newpath)
+        misc.imsave(image_file[0:-4] + '_col' + str(1) + '.jpg', img_mod1)
 
-print('OK!')
+        #print 'image ' + image_file[0:-4] + '_col' + str(1) + '.jpg created \n'
+        os.chdir(mypath)
+
+        img_mod2, image_file = changecolour(f, np.random.random([1]),np.random.random([1]),np.random.random([1]) )
+        os.chdir(newpath)
+        misc.imsave(image_file[0:-4] + '_col' + str(2) + '.jpg', img_mod2)
+
+        #print 'image ' + image_file[0:-4] + '_col' + str(2) + '.jpg created \n'
+        os.chdir(mypath)
+
+        img_mod3, image_file = saltpepper(f, np.random.random([1]) * 0.05)
+        os.chdir(newpath)
+        misc.imsave(image_file[0:-4] + '_noise' + '.jpg', img_mod3)
+        #print 'image ' + image_file[0:-4] + '_noise' + '.jpg created \n'
+        os.chdir(mypath)
+
+    print('OK!')
 
 
-
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    Colors_noise(filename)
 
 
 
