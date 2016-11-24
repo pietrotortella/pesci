d6193aed798963110781b5394c0f0b6b70ee1e91
carlotardivo <carlo.tardivo@gmail.com>
2016-11-24 09:27:57 +0100

    The fist script is to select a given a percentage of images uniformly distributed beteween class types.
    The second is to setting the filename paths to one passed from command line and possibly compatible with each own machine.

---
+++ code/Random_select_annotations.py
@@ -0,0 +1,48 @@
+import json,os,sys,random
+from collections import Counter
+
+
+def random_select_annotations(jsonInput,percentage,jsonOutput):
+
+    # Extracting data from json input
+    with open(jsonInput) as data_file:
+        data = json.load(data_file)
+
+    # List of [index,label] elements
+    label_list = []
+    for index, dict_it in enumerate(data):
+        label_list.append([index, os.path.basename(os.path.dirname(dict_it['filename']))])
+
+    # List of all label occurrences
+    all_labels = [x[1] for x in label_list]
+
+    # Dictionary with number of occurrences for each label
+    howmany = Counter(all_labels)
+
+    # List of labels
+    labels = list(set(i for i in all_labels))
+
+    # Creation of the new list
+    newlist = []
+    for l in labels:
+        n = int(percentage * howmany[l])
+        print 'Selecting', n, 'elements of', l, 'type'
+        # Extract n dictionaries with file of type l
+        indices = [x[0] for x in label_list if x[1] == l]
+        selectedindices = random.sample(indices, n)
+        for i in selectedindices:
+            newlist.append(data[i])
+
+    print 'Number of total inputs selected = ', len(newlist)
+
+    with open(jsonOutput,'w') as f:
+        json.dump(newlist,f,indent=4)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    jsoninput = sys.argv[1]
+    p = sys.argv[2]
+    jsonoutput = sys.argv[3]
+    random_select_annotations(jsoninput,p,jsonoutput)
+
