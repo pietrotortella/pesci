0d7b18603b51e80a63392369b00858edb1054b63
claudiatin <claudia.tinnirello.com>
2016-11-30 15:48:24 +0100

    Merge branch 'master' of /home/terminale4/Desktop/pesci with conflicts.

--- Detection/DetectionDatabase_from_json.py
+++ Detection/DetectionDatabase_from_json.py
@@ -3,7 +3,7 @@
 import json
 
 import cv2
-
+import math
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -11,9 +11,9 @@ import matplotlib.pyplot as plt
 #-----------------------------------------
 #Getting the target set from the json file
 
-
-json_path = "/annotations/TIN_DOL.json"
-mypath = "/home/claudia/Kaggle_challenge/train/train/"
+json_path="/home/terminale4/Desktop/kaggle_FishCompetition/train/train/annotations/ALL_clean.json"
+#json_path = "/home/terminale4/Desktop/kaggle_FishCompetition/train/train/annotations/TIN_DOL.json"
+mypath = "/home/terminale4/Desktop/kaggle_FishCompetition/train/train/"
 jsn_file = open(json_path).read()
 jsn_data = json.loads(jsn_file)
 
@@ -23,27 +23,27 @@ def getData(jsn_data):
         file = mypath+jsn_data[i]["filename"][2:]
         img = cv2.imread(file)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img, (4, 4))
+        img = cv2.resize(img, (96, 96))
         img = [item for sublist in img for item in sublist]
         l.append(img)
     return l
 
 data = getData(jsn_data)
-print data
+print len(data)
 
 
-#---------------------
-#Getting the target set from the json file
-
-
+# #---------------------
+# #Getting the target set from the json file
+#
+#
 def annot_dict(annots,label):
     list = [annot for annot in annots if annot["class"] == label]
     return list[0]
-
-
+#
+#
 def getTarget(jsn_data):
     l=[]
-    for i in range(3):
+    for i in range(len(jsn_data)):
         a = []
         annots = jsn_data[i]["annotations"]
         a.append(int(annot_dict(annots,"fish")["x"]))
@@ -67,5 +67,50 @@ def getTarget(jsn_data):
 
 
 target=getTarget(jsn_data)
-print target
+#print target
+
+
+def getFish(jsn_data):
+    l = []
+    for i in range(len(jsn_data)):
+        file = mypath+jsn_data[i]["filename"][2:]
+        annots = jsn_data[i]["annotations"]
+        x = int(annot_dict(annots, "fish")["x"])
+        y = int(annot_dict(annots, "fish")["y"])
+        h = int(annot_dict(annots, "fish")["height"])
+        w = int(annot_dict(annots, "fish")["width"])
+        img = cv2.imread(file)
+        img = img[y:y + h, x:x + w]
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (96, 96))
+        img = [item for sublist in img for item in sublist]
+        l.append(img)
+    return l
+
+
+data_fish=getFish(jsn_data)
+print len(data_fish)
+
+
+def getAngleTarget(jsn_data):
+    thetas=[]
+    for i in range(len(jsn_data)):
+        annots = jsn_data[i]["annotations"]
+        yhead=annot_dict(annots, "head")["y"]
+        xhead=annot_dict(annots, "head")["x"]
+        ytail=annot_dict(annots, "tail")["y"]
+        xtail=annot_dict(annots, "tail")["x"]
+        slope = (yhead - ytail) / (xhead - xtail+ 10 **-8)
+        theta = math.degrees(math.atan(slope))
+        thetas.append(theta)
+    return thetas
+
+angle_target = getAngleTarget(jsn_data)
+print len(angle_target)
+
+
+
+
+
+
 
